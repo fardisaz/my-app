b0c16cc7111e9301940aae3d67fcde7796b33306
fardisaz <amoozadeh73@yahoo.com>
2021-02-08 08:59:27 +0100

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page class="flex flex-center ">
+        <div v-for="arr in newArr" :key="arr.bgColor" class="q-pa-sm flex">
+      <q-checkbox
+        v-model="arr.checked"
+        keep-color
+        :label="arr.bgColor"
+        :style="{ color: '#' + arr.bgColor }"
+      ></q-checkbox>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+ 
+  
+  data(){
+return{
+ 
+   newArr: [
+        {
+          bgColor: "501253",
+          checked: false
+        },
+        {
+          bgColor: "acedef",
+          checked: false
+        },
+        {
+          bgColor: "deadbeef",
+          checked: false
+        }
+      ]
+  
+}
+  }
+}
+</script>
+<style scoped >
+
+
+.q-checkbox__inner {
+  color: unset;
+}
+.q-checkbox__label {
+  color: black;
+}
+.q-checkbox__inner .q-checkbox__bg {
+    background: currentColor;
+}
+</style>
